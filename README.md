1a8ce42617043217cf4c14d4bc3e755265e29c9e
Tomasz Klim <github@tomaszklim.pl>
2016-03-16 18:38:01 +0100

    first version

---
+++ README.md
@@ -0,0 +1,3 @@
+sf-mta-forwarder extension provides minimal MTA functionality, forwarding
+all messages generated on the localhost to other host in LAN (which uses
+either sf-mta-relay extension, or some custom MTA configuration).
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+. /opt/farm/scripts/init
+. /opt/farm/scripts/functions.custom
+. /opt/farm/scripts/functions.install
+
+
+if [ "$SMTP" = "true" ]; then
+	echo "install sf-mta-relay extension instead of sf-mta-forwarder"
+	exit 0
+fi
+
+DOMAIN=`external_domain`
+
+common=/opt/sf-mta-forwarder/templates
+base=$common/$OSVER
+
+if [ -f $base/postfix.tpl ]; then
+	install_rpm postfix
+	install_rpm mailx
+	save_original_config /etc/postfix/main.cf
+
+	echo "setting up postfix"
+	cat $base/postfix.tpl |sed -e s/%%host%%/$HOST/g -e s/%%domain%%/$DOMAIN/g -e s/%%smtp%%/$SMTP/g >/etc/postfix/main.cf
+
+	echo "setting up mail aliases"
+	cat $common/aliases-$OSTYPE.tpl |sed -e s/%%host%%/$HOST/g -e s/%%domain%%/$DOMAIN/g >/etc/aliases
+	newaliases
+
+	service postfix reload
+
+elif [ "$OSTYPE" = "debian" ]; then
+	install_deb ssmtp
+	install_deb bsd-mailx
+
+	echo "setting up ssmtp"
+	cat $common/ssmtp.tpl |sed -e s/%%host%%/$HOST/g -e s/%%domain%%/$DOMAIN/g -e s/%%smtp%%/$SMTP/g >/etc/ssmtp/ssmtp.conf
+fi
