af7dc9e25e90318dcd4f1a590025be28b56592a9
Tomasz Klim <github@tomaszklim.pl>
2019-07-23 14:05:07 +0200

    fixes to allow clean replacement by postfix

--- setup-lsb-invalid-mta.sh
+++ setup-lsb-invalid-mta.sh
@@ -5,11 +5,11 @@
 
 if [ -f /etc/debian_version ]; then
 	/opt/farm/ext/packages/utils/install.sh libgnutls-openssl27
+	/opt/farm/ext/packages/utils/uninstall.sh postfix exim4 exim4-daemon-light exim4-base exim4-config
 
 	echo "checking for debian package lsb-invalid-mta"
 	if [ "`dpkg -l lsb-invalid-mta 2>/dev/null |grep ^ii`" = "" ] \
-	&& [ "`dpkg -l ssmtp 2>/dev/null |grep ^ii`" = "" ] \
-	&& [ "`dpkg -l postfix 2>/dev/null |grep ^ii`" = "" ]; then
+	&& [ "`dpkg -l ssmtp 2>/dev/null |grep ^ii`" = "" ]; then
 		echo "installing lsb-invalid-mta from provided deb package"
 		dpkg -i /opt/farm/ext/mta-forwarder/support/packages/lsb-invalid-mta_4.1+Debian13+nmu1_all.deb
 	fi
@@ -36,8 +36,8 @@ if [ -f /etc/debian_version ]; then
 		arch=`/opt/farm/ext/system/detect-architecture.sh`
 		ln -sf /opt/farm/ext/mta-forwarder/support/ssmtp_debian/ssmtp.$arch /usr/sbin/ssmtp
 
-		ln -sf /usr/sbin/ssmtp /usr/sbin/mailq
-		ln -sf /usr/sbin/ssmtp /usr/sbin/newaliases
+		ln -sf /usr/sbin/ssmtp /usr/bin/mailq
+		ln -sf /usr/sbin/ssmtp /usr/bin/newaliases
 		ln -sf /usr/sbin/sendmail /usr/lib/sendmail
 
 		if [ ! -s /etc/debsums-ignore ] || ! grep -q /usr/sbin/sendmail /etc/debsums-ignore; then
